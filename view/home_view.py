0732ce2883eddb3e691f67bbd7b341eab036490a
José Henrique <109696678+atm-jose@users.noreply.github.com>
2025-10-07 12:58:14 -0400

    Initital implementation of landing workflow

--- view/home_view.py
+++ view/home_view.py
@@ -1,13 +1,22 @@
 import dearpygui.dearpygui as dpg
 from gettext import gettext as _
-
-def save_callback():
-    print("Save Clicked")
     
 class HomeView:
     def __init__(self):
-        with dpg.window(label=_("Window Exemple")):
-            dpg.add_text("Hello world")
-            dpg.add_button(label="Save", callback=save_callback)
-            dpg.add_input_text(label="string")
-            dpg.add_slider_float(label="float")
+        self.menu_bar()
+    
+    def menu_bar(self):
+        with dpg.viewport_menu_bar():
+            with dpg.menu(label=_("File")):
+                dpg.add_menu_item(label="Save")
+                dpg.add_menu_item(label="Save As")
+            with dpg.menu(label=_("view")):
+                dpg.add_menu_item(label=_("View Simulation Window"), callback=self.simulation_window)
+                dpg.add_menu_item(label=_("View Equation Window"))
+
+    def simulation_window(self):
+        with dpg.window(label=_("Simulation Window"), tag="simulation_window"):
+            dpg.add_text("Simulation Content Here")
+        dpg.set_item_width("simulation_window", 800)
+        dpg.set_item_height("simulation_window", 400)
+        dpg.set_item_pos("simulation_window", [200, 200])
