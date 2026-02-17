9214894ec2c4da72bbd51c10f72e856e8b809635
jose-henrique <clanosbrasil2003@gmail.com>
2026-02-16 21:08:56 -0400

    Ajustando controller

--- controller/fin_analyses_controller.py
+++ controller/fin_analyses_controller.py
@@ -6,16 +6,13 @@ class FinAnalysesController:
         pass
     
     def solve_analyses(self, params):
-        #solver = FinSolver(params["fin_geometry"], params["solver_method"])
-        #data = self.__clean_data(params["data"])
-        #temp_array = solver.find_local_temperature(solver.find_temp_distribuition(data), params["env_temperature"], params["base_temperature"])
-        #return {'temperatures': temp_array, 'base_temperature': params["base_temperature"]}
-        solver = FinSolver(2, 1)
-        data = {'convection_coefficient': 15.0, 'dimensions': {'radius': 0.003}, 'fin_length': 0.008, 'node_count': 10, 'fin_material': 'copper'}
+        solver = FinSolver(params["fin_geometry"], params["solver_method"])
+        data = self.__clean_data(params["data"])
+        temp_array = solver.find_local_temperature(solver.find_temp_distribuition(data), params["env_temperature"], params["base_temperature"])
+        
+        
         temp_distribuition = solver.find_temp_distribuition(data)
         if temp_distribuition:
-            print("sucesso")
-            temp_array = solver.find_local_temperature(temp_distribuition, 25, 80)
             return {'temperatures': temp_array, 'base_temperature': 80, 'status': 0}
         else:
             return {'errors': solver.errors, 'status': -1}
