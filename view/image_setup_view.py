3fcbd47f378aeefc71655c8d0d6240885e88aa1d
jose-henrique <clanosbrasil2003@gmail.com>
2026-03-12 22:27:45 -0400

    adicionando campos ao modal de configuração das imagens

--- view/image_setup_view.py
+++ view/image_setup_view.py
@@ -1,6 +1,7 @@
 import dearpygui.dearpygui as dpg
 from gettext import gettext as _
 from model.utils.font_manager import FontManager
+import time
 
 class ImageSetupView():
     def __init__(self, canva):
@@ -23,6 +24,7 @@ class ImageSetupView():
             height=self.height_window,
             pos=[self.pos_x,self.pos_y]):
                 self.__render_options()
+                self.__get_folder_location()
                 dpg.bind_item_font("button_folder",icons_font)
     
     def __render_options(self):
@@ -30,7 +32,15 @@ class ImageSetupView():
             dpg.add_text(_("File Location"))
             with dpg.group(horizontal=True):
                 dpg.add_input_text(label="", default_value="C:/users/jose.ribeiro/images", tag="location", readonly=True, width=200,enabled=True)
-                dpg.add_button(label="\uf07b", width=75, tag="button_folder", callback=self.__get_folder_location)
+                dpg.add_button(label="\uf07b", width=75, tag="button_folder", callback=self.__show_folder_selector)
+        dpg.add_spacer(height=7)
+        with dpg.group(tag="name_group"):
+            dpg.add_text(_("Image Name"))
+            dpg.add_input_text(label="", default_value=f"{time.time()}", tag="filename", width=self.width_window-20,enabled=True)
+        dpg.add_spacer(height=7)
+        with dpg.group(tag="size_group", width=self.width_window-20):
+            dpg.add_text(_("Image Resolution"))
+            dpg.add_combo(["SD (640x480)","HD (1280x720)","FULL HD (1920x1080)","QUAD HD (2560x1440)","4k (3480x2160)"], default_value=_("Select A Resolution"), tag="image_size")
             
         
         self.__bottom_window()
@@ -39,24 +49,22 @@ class ImageSetupView():
     def __bottom_window(self):
         with dpg.group(parent=self.window_name, horizontal=True):
             dpg.add_separator(parent=self.window_name)
-            dpg.add_button(label=_("SAVE"), width=75, callback=self.__save_image, parent=self.window_name, indent=self.width_window - 120)
+            dpg.add_button(label=_("SAVE"), width=75, callback=self.__save_image, parent=self.window_name, indent=self.width_window - 100)
             
     def __get_folder_location(self):
-        print("hello")
         dpg.add_file_dialog(
-                directory_selector=True, show=True, callback=self.__callback, tag="file_dialog_id",
+                directory_selector=True, show=False, callback=self.__callback, tag="file_dialog_id",
                 cancel_callback=self.__cancel_callback, width=700 ,height=400)
         
+    def __show_folder_selector(self):
+        dpg.show_item("file_dialog_id")
+        
     
-    def __callback(sender, app_data):
-        print('OK was clicked.')
-        print("Sender: ", sender)
-        print("App Data: ", app_data['file_path_name'])
+    def __callback(self, sender, app_data):
+        print(app_data["file_path_name"])
 
-    def __cancel_callback(sender, app_data):
-        print('Cancel was clicked.')
-        print("Sender: ", sender)
-        print("App Data: ", app_data)    
+    def __cancel_callback(self, sender, app_data):
+            dpg.hide_item("file_dialog_id")
             
     def __save_image(self):
         self.canva.save_image()
