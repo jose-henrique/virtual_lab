5aedf01cb29aec19febf3b375e0686ad67a3e25a
jose-henrique <clanosbrasil2003@gmail.com>
2026-03-12 21:53:09 -0400

    Implementacao do gerenciador global de fontes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import dearpygui.dearpygui as dpg
 from gettext import gettext as _
 from controller.application_controller import ApplicationController
+from model.utils.font_manager import FontManager
 
 dpg.create_context()
+
+fm = FontManager()
+fm.load_all_fonts()
+
 dpg.create_viewport(title="Virtual Lab")
 dpg.setup_dearpygui()
 
--- model/utils/font_manager.py
+++ model/utils/font_manager.py
@@ -1,9 +1,10 @@
 import dearpygui.dearpygui as dpg   
-import os
+from pathlib import Path
 
 class FontManager:
     _instance = None
     fonts = {}
+    target_sizes = [{"name": "small", "size": 12}, {"name": "base", "size": 15}, {"name": "medium", "size": 24}, {"name": "large", "size": 28}]
 
 
     def __new__(cls):
@@ -13,11 +14,46 @@ class FontManager:
     
 
     def load_all_fonts(self):
-        root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        for subdir, dirs, files in os.walk(os.path.join(root,"fonts")):
-            for file in files:
-                print('Loading font: ', file)
+        root = Path(__file__).resolve().parents[2]
+        fonts_dir = root / "fonts"
+        with dpg.font_registry():
+            for font_file in fonts_dir.rglob("*"):
+                try:
+                    extension = font_file.suffix.lower()
 
+                    if extension == ".otf":
+                        self.__add_icons_font(font_file)
+                    
+                    if extension == ".ttf":
+                        self.__add_text_font(font_file)
+                    
+                    
+                except Exception as e:
+                    print(f"Failed to load {font_file}: {e}")  
+                    
+    def get(self, name):
+        return self.fonts.get(name)
+    
+    def __add_icons_font(self, font_file):
+        file_name = font_file.stem
+        suffix = file_name.split("-")[-1]
+        self.__generate_size_variation(f"icons_{suffix}", font_file, icon=True)
+        
+    def __add_text_font(self, font_file):
+        file_name = font_file.stem
+        prefix = file_name.split("-")[0].lower()
+        self.__generate_size_variation(f"text_{prefix}", font_file)
+    
+    
+    def __generate_size_variation(self, prefix, font_file, icon=False):
+        for size_info in self.target_sizes:
+            font_nickname = f"{prefix}_{size_info["name"]}"
+            font_id = dpg.add_font(str(font_file), size_info["size"])
+            
+            if icon:
+                dpg.add_font_range(0xf000, 0xf900, parent=font_id)
+            
+            self.fonts[font_nickname] = font_id
+        
 
-fm = FontManager()
-fm.load_all_fonts()
+    
